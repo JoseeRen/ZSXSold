45ee83ccce89090953dac9d426cceb9dcac10827
RenYaoWei <renyaowei@foxmail.com>
2017-06-08 15:43:00 +0800

    任耀威的Activity

---
+++ app/src/main/java/com/ryw/zsxs/activity/RywActivity.java
@@ -0,0 +1,18 @@
+/*
+ * Create on 2017-6-8 下午3:42
+ * FileName: RywActivity.java
+ * Author: Ren Yaowei
+ * Blog: http://www.renyaowei.top
+ * Email renyaowei@foxmail.com
+ */
+
+package com.ryw.zsxs.activity;
+
+import android.app.Activity;
+
+/**
+ * Created by Mr_Shadow on 2017/6/8.
+ */
+
+public class RywActivity extends Activity {
+}
