0819944e9b3275127e320f690f11ec99ad438ed1
RenYaoWei <renyaowei@foxmail.com>
2017-06-08 14:58:00 +0800

    基本框架

---
+++ app/src/main/java/com/ryw/zsxs/activity/MainActivity.java
@@ -0,0 +1,26 @@
+/*
+ * Create on 2017-6-8 下午2:18
+ * FileName: MainActivity.java
+ * Author: Ren Yaowei
+ * Blog: http://www.renyaowei.top
+ * Email renyaowei@foxmail.com
+ */
+
+package com.ryw.zsxs.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ryw.zsxs.R;
+
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+}
