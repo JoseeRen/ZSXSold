06abf09d869b0b7fbec6115c6289b7ff0ae57b6a
RenYaoWei <renyaowei@foxmail.com>
2017-06-08 15:50:37 +0800

    任耀威的Activity

--- app/src/main/java/com/ryw/zsxs/activity/RywActivity.java
+++ app/src/main/java/com/ryw/zsxs/activity/RywActivity.java
@@ -9,10 +9,16 @@
 package com.ryw.zsxs.activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
 /**
  * Created by Mr_Shadow on 2017/6/8.
  */
 
 public class RywActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
