a0d47148c85b85d055ce81562469e368f21b39db
Zm <2390235300@qq.com>
2017-06-10 19:48:24 +0800

    这里写你都 做了什么，改了哪里

---
+++ app/src/main/java/com/ryw/zsxs/activity/ZmActivity.java
@@ -0,0 +1,27 @@
+/*
+ * Create on 2017-6-8 下午8:22
+ * FileName: ZmActivity.java
+ * Author: Ren Yaowei
+ * Blog: http://www.renyaowei.top
+ * Email renyaowei@foxmail.com
+ */
+
+package com.ryw.zsxs.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by Zhao on 2017/6/8.
+ */
+
+public class ZmActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.e("acd","acd");
+    }
+
+}
