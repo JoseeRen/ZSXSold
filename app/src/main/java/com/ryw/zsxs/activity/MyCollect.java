5ecdd9700a44d9730cd306bfb4ed3265fc5bc967
Zm <2390235300@qq.com>
2017-06-24 17:56:41 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	pulltorefresh_library/build/intermediates/incremental/mergeDebugAndroidTestResources/compile-file-map.properties
    #	pulltorefresh_library/build/intermediates/incremental/packageDebugResources/compile-file-map.properties
    #	pulltorefresh_library/build/intermediates/incremental/packageReleaseResources/compile-file-map.properties
    #	pulltorefresh_library/build/outputs/aar/pulltorefresh_library-debug.aar
    #	pulltorefresh_library/build/outputs/aar/pulltorefresh_library-release.aar

--- app/src/main/java/com/ryw/zsxs/activity/MyCollect.java
+++ app/src/main/java/com/ryw/zsxs/activity/MyCollect.java
@@ -3,17 +3,30 @@ package com.ryw.zsxs.activity;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
+import com.google.gson.Gson;
 import com.ryw.zsxs.R;
+import com.ryw.zsxs.app.Constant;
 import com.ryw.zsxs.base.BaseActivity;
+import com.ryw.zsxs.bean.MyCollectBean;
+import com.ryw.zsxs.utils.SpUtils;
+import com.ryw.zsxs.utils.XutilsHttp;
 import com.ryw.zsxs.view.MyViewpager;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.StringTokenizer;
+import java.util.zip.Inflater;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -41,11 +54,7 @@ public class MyCollect extends BaseActivity {
     @BindView(R.id.mycollect_vp)
     MyViewpager mycollectVp;
     private ArrayList<RadioButton> rb;
-
-
-
-
-
+    private List<MyCollectBean.CourseBean> course;
 
     public int getContentViewResId() {
         return R.layout.activity_mycollect;
@@ -57,7 +66,6 @@ public class MyCollect extends BaseActivity {
     }
 
 
-
     private void initData() {
         rb = new ArrayList<>();
         rb.add(rbShipin);
@@ -65,10 +73,26 @@ public class MyCollect extends BaseActivity {
         rb.add(rbDushu);
         rb.add(rbWenzhang);
 
-        mycollectVp.setAdapter(new MyPagerAdapter());
-        mycollectVp.setOnPageChangeListener(new MyOnPageChangeListener());
-    }
 
+        mycollectVp.setOnPageChangeListener(new MyOnPageChangeListener());
+
+        HashMap<String, String> hashmap = new HashMap<>();
+        hashmap.put("Action", "getFavorite");
+//        hashmap.put("acode", SpUtils.getString(mContext,LoginAcitvity.ACODE));
+//        hashmap.put("Uid",SpUtils.getString(mContext,LoginAcitvity.USERNAME));
+        hashmap.put("acode", "280d546cc83ab2140127b3a09b0ee265");//这里以后需要改的
+        hashmap.put("Uid", "18733513882");
+        XutilsHttp.getInstance().get(Constant.HOSTNAME, hashmap, new XutilsHttp.XCallBack() {
+            @Override
+            public void onResponse(String result) {
+                Gson gson = new Gson();
+                MyCollectBean myCollectBean = gson.fromJson(result, MyCollectBean.class);
+                course = myCollectBean.Course;
+                Log.e("huida", course.get(0).title);
+                mycollectVp.setAdapter(new MyPagerAdapter());
+            }
+        });
+    }
 
 
     //viewpager页面滑动的监听，用来实现上面按钮的选择
@@ -90,8 +114,6 @@ public class MyCollect extends BaseActivity {
     }
 
 
-
-
     //viewpager的填充
     private class MyPagerAdapter extends PagerAdapter {
         @Override
@@ -101,12 +123,17 @@ public class MyCollect extends BaseActivity {
 
         @Override
         public boolean isViewFromObject(View view, Object object) {
-            return view==object;
+            return view == object;
         }
 
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
             View view = View.inflate(mContext, R.layout.mycollect_list_item, null);
+            ListView mycollect_lv = view.findViewById(R.id.mycollect_lv);
+
+            mycollect_lv.setAdapter(new MyCollectLvAdapter());
+
+
             container.addView(view);
             return view;
         }
@@ -116,9 +143,61 @@ public class MyCollect extends BaseActivity {
             container.removeView((View) object);
 
         }
+
+        private class MyCollectLvAdapter extends BaseAdapter {
+            @Override
+            public int getCount() {
+                return course.size();
+            }
+
+            @Override
+            public Object getItem(int i) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int i) {
+                return 0;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                ViewHolder holder = null;
+                View view;
+                if (convertView == null) {
+                    view = View.inflate(mContext, R.layout.item_xuankedetail_pvlistview, null);
+                    holder = new ViewHolder();
+                    holder.iv_item_xuankedetail_pv = view.findViewById(R.id.iv_item_xuankedetail_pv);
+                    holder.tv_title_item_xuankedetail_pv = view.findViewById(R.id.tv_title_item_xuankedetail_pv);
+                    holder.tv_keshi_item_xuankedetail_pv = view.findViewById(R.id.tv_keshi_item_xuankedetail_pv);
+                    holder.tv_jifen_item_xuankedetail_pv = view.findViewById(R.id.tv_jifen_item_xuankedetail_pv);
+                    holder.tv_info_item_xuankedetail_pv=view.findViewById(R.id.tv_info_item_xuankedetail_pv);
+                    holder.tv_dianjiliang_item_xuankedetail_pv = view.findViewById(R.id.tv_dianjiliang_item_xuankedetail_pv);
+                    view.setTag(holder);
+
+                } else {
+                    view = convertView;
+                    holder = (ViewHolder) view.getTag();
+                }
+
+                XutilsHttp.getInstance().bindCommonImage(holder.iv_item_xuankedetail_pv, course.get(position).img, true);
+                holder.tv_title_item_xuankedetail_pv.setText(course.get(position).title);
+                holder.tv_keshi_item_xuankedetail_pv.setText(course.get(position).keshi+"");
+                holder.tv_info_item_xuankedetail_pv.setText(course.get(position).info);
+                holder.tv_jifen_item_xuankedetail_pv.setText(course.get(position).money+"");
+                holder.tv_dianjiliang_item_xuankedetail_pv.setText(course.get(position).hot+"");
+
+                return view;
+            }
+
+            class ViewHolder {
+                public ImageView iv_item_xuankedetail_pv;
+                public TextView tv_title_item_xuankedetail_pv, tv_info_item_xuankedetail_pv,tv_keshi_item_xuankedetail_pv, tv_jifen_item_xuankedetail_pv, tv_dianjiliang_item_xuankedetail_pv;
+
+
+            }
+        }
     }
-
-
 
 
 }
--- app/src/main/java/com/ryw/zsxs/bean/MyCollectBean.java
+++ app/src/main/java/com/ryw/zsxs/bean/MyCollectBean.java
@@ -1,8 +1,45 @@
 package com.ryw.zsxs.bean;
 
+import java.util.List;
+
 /**
  * Created by Zhao on 2017/6/24.
  */
 
 public class MyCollectBean {
+
+    /**
+     * page_all : 1
+     * page_now : 1
+     * Course : [{"kc_id":"140725","title":"2017骞村娓歌\u20ac冭瘯銆愬ぇ绾插垎鏋愩\u20ac戣搴�","img":"http://www.chinaplat.com/CourseImg/IMG-20170509/217_156_20170509142596339633.png","info":"2017骞村娓歌\u20ac冭瘯銆愬ぇ绾插垎鏋愩\u20ac戣搴�","money":"2900","keshi":4,"hot":676,"teacher":"闄堥緳"}]
+     */
+
+    public String page_all;
+    public String page_now;
+    public List<CourseBean> Course;
+
+    public static class CourseBean {
+        /**
+         * kc_id : 140725
+         * title : 2017骞村娓歌€冭瘯銆愬ぇ绾插垎鏋愩€戣搴�
+         * img : http://www.chinaplat.com/CourseImg/IMG-20170509/217_156_20170509142596339633.png
+         * info : 2017骞村娓歌€冭瘯銆愬ぇ绾插垎鏋愩€戣搴�
+         * money : 2900
+         * keshi : 4
+         * hot : 676
+         * teacher : 闄堥緳
+         */
+
+        public String kc_id;
+        public String title;
+        public String img;
+        public String info;
+        public String money;
+        public int keshi;
+        public int hot;
+        public String teacher;
+
+        
+        
+    }
 }
