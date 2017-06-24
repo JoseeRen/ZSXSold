f31aeee7595c1de3b70d16328a53dae75bfeaf6b
yicez <694034118@qq.com>
2017-06-24 16:18:37 +0800

    杨青activity

---
+++ app/src/main/java/com/ryw/zsxs/activity/UserEmailActivity.java
@@ -0,0 +1,22 @@
+package com.ryw.zsxs.activity;
+
+import android.os.Bundle;
+
+import com.ryw.zsxs.R;
+import com.ryw.zsxs.base.BaseActivity;
+
+/**
+ * Created by Y.Q on 2017/6/24.
+ */
+
+public class UserEmailActivity extends BaseActivity {
+    @Override
+    public int getContentViewResId() {
+        return R.layout.activity_useremail;
+    }
+
+    @Override
+    public void init(Bundle savedInstanceState) {
+
+    }
+}
---
+++ app/src/main/java/com/ryw/zsxs/activity/UserNicknameActivity.java
@@ -0,0 +1,23 @@
+package com.ryw.zsxs.activity;
+
+import android.os.Bundle;
+
+import com.ryw.zsxs.R;
+import com.ryw.zsxs.base.BaseActivity;
+
+/**
+ * Created by Y.Q on 2017/6/24.
+ * 个人信息中昵称页面
+ */
+
+public class UserNicknameActivity extends BaseActivity {
+    @Override
+    public int getContentViewResId() {
+        return R.layout.activity_usernickname;
+    }
+
+    @Override
+    public void init(Bundle savedInstanceState) {
+
+    }
+}
