0e9aecdaff7f776b4540e79daca7653c5c2d427b
Zm <2390235300@qq.com>
2017-06-24 09:49:48 +0800

    adasfsad

---
+++ app/src/main/java/com/ryw/zsxs/activity/MyCollect.java
@@ -0,0 +1,16 @@
+package com.ryw.zsxs.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Zhao on 2017/6/24.
+ */
+
+public class MyCollect extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
