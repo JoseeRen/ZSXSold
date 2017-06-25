9dd8c77b8d3d2c2cb534d599b704ccf183e0a4da
zhaogui <954923610@qq.com>
2017-06-25 15:12:47 +0800

    111

---
+++ app/src/main/java/com/ryw/zsxs/bean/UserInfoBean.java
@@ -0,0 +1,33 @@
+package com.ryw.zsxs.bean;
+
+/**
+ * Created by gui on 2017/6/25.
+ */
+
+public class UserInfoBean {
+
+    /**
+     * Nicename : 鍧戠埞鐨�
+     * Sex : 
+     * Address : 
+     * Mobile : 
+     * Email : email
+     * Pic : http://www.chinaplat.com/User/UserHeadImg/164151.jpg
+     * Jifen : 598
+     * Money : 0
+     * xueshi : 0
+     */
+
+    public String Nicename;
+    public String Sex;
+    public String Address;
+    public String Mobile;
+    public String Email;
+    public String Pic;
+    public String Jifen;
+    public String Money;
+    public int xueshi;
+
+ 
+  
+}
